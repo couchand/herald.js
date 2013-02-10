b725c77203bbef6a5d7c6b6863c4c5822a691419
Andrew Couch <andy@couchand.com>
2013-02-10 14:55:50 -0500

    creation.

--- test/herald_test.js
+++ test/herald_test.js
@@ -1,12 +1,17 @@
+/*jshint expr: true */
 /*globals describe: true, it: true */
 "use strict";
 
 var herald = require('../lib/herald.js');
 
-describe('Herald', function(){
-  describe('herald()', function(){
-    it('is a function', function(){
+describe('Herald', function() {
+  describe('herald()', function() {
+    it('is a function', function() {
       herald.should.be.a('function');
+    });
+
+    it('creates new Heralds', function() {
+      herald().should.be.ok;
     });
   });
 });
