469826aff0aa3b0a02b15704ef25b9498eecbeac
Andrew Couch <andy@couchand.com>
2013-02-10 15:11:38 -0500

    finally some initial development on heralds

--- test/herald_test.js
+++ test/herald_test.js
@@ -1,5 +1,5 @@
 /*jshint expr: true */
-/*globals describe: true, it: true */
+/*globals describe: true, it: true, before: true */
 "use strict";
 
 var herald = require('../lib/herald.js');
@@ -12,6 +12,36 @@ describe('Herald', function() {
 
     it('creates new Heralds', function() {
       herald().should.be.ok;
+    });
+
+    it('creates them `awaiting`', function() {
+      var myHerald = herald();
+      myHerald.awaiting().should.be.ok;
+    });
+  });
+
+  describe('#', function() {
+    var myHerald;
+
+    before(function() {
+      myHerald = herald();
+    });
+
+    describe('awaiting()', function() {
+      it('is a function', function() {
+        myHerald.awaiting.should.be.a('function');
+      });
+    });
+
+    describe('dispatch()', function() {
+      it('is a function', function() {
+        myHerald.dispatch.should.be.a('function');
+      });
+
+      it('stops waiting after dispatch', function() {
+        myHerald.dispatch();
+        myHerald.awaiting().should.not.be.ok;
+      });
     });
   });
 });
